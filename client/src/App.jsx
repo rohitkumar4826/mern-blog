f95408bb0f963615033c417418e8715daad6678f
rohitkumar4826 <supervikalpa@gmail.com>
2024-06-21 11:55:13 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
 function App() {
   return (
     <>
-      <div className="font-bold">
-        Geeks for geeks
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>} />
+          <Route path='signin' element={<Signin/>} />
+          <Route path='/signup' element={<Signup/>} />
+          <Route path='/dashboard' element={<Dashboard/>} />
+          <Route path='/projects' element={<Projects/>} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
